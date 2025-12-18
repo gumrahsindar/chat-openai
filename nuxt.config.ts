c5d42c14267614f1dbedf2024756356efa02c001
gumrahsindar <gsindar@gmail.com>
2025-12-18 10:35:09 +0300

    feat: add nitro storage configuration for production and development environments

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
   },
 
-  modules: ['@nuxt/eslint'],
-})
+  modules: ["@nuxt/eslint"],
+
+  nitro: {
+    storage: {
+      db: {
+        driver: "fs",
+        base: "./.data",
+      },
+    },
+  },
+
+  $production: {
+    nitro: {
+      storage: {
+        db: {
+          driver: "netlify-blobs",
+          name: "db",
+        },
+      },
+    },
+  },
+});
