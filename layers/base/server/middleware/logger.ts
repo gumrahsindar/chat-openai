0295d98c5accce06263d17d80deef5dd3cc89857
gumrahsindar <gsindar@gmail.com>
2025-12-18 14:01:43 +0300

    Refactor code structure for improved readability and maintainability

--- layers/base/server/middleware/logger.ts
+++ layers/base/server/middleware/logger.ts
@@ -1,8 +1,8 @@
-export default defineEventHandler(async (event) => {
-  const storage = useStorage("db");
-  await storage.setItem(`telemetry:request:${Date.now()}`, {
-    url: getRequestURL(event),
-    method: event.method,
-    headers: getRequestHeaders(event),
-  });
+export default defineEventHandler(async (_event) => {
+  // const storage = useStorage("db");
+  // await storage.setItem(`telemetry:request:${Date.now()}`, {
+  //   url: getRequestURL(event),
+  //   method: event.method,
+  //   headers: getRequestHeaders(event),
+  // });
 });
