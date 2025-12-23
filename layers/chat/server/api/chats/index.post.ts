a863a3c371ec08b180648a69238d2539b0db2144
gumrahsindar <gsindar@gmail.com>
2025-12-23 22:16:14 +0300

    feat: enhance chat fetching and creation handlers with caching and logging

--- layers/chat/server/api/chats/index.post.ts
+++ layers/chat/server/api/chats/index.post.ts
@@ -1,9 +1,12 @@
-import { createChat } from '../../repository/chatRepository'
+import { createChat } from "../../repository/chatRepository";
 
 export default defineEventHandler(async (event) => {
-  const { title, projectId } = await readBody(event)
+  const { title, projectId } = await readBody(event);
+
+  const storage = useStorage("db");
+  await storage.setItem("chats:has-new-chat", true);
   return createChat({
     title,
     projectId,
-  })
-})
+  });
+});
